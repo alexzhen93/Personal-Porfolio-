13ce9181b550d91b53a56d3135d7487c88eec31a
alexzhen93 <alex_15_zhen@hotmail.com>
2020-06-12 23:05:59 -0400

    About Page Content add

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
 
     return(
-        <p>AboutPage Work!</p>
+        <div>
+            <Hero title={props.title}/>
+            <Content>
+                <p>Hello, my name is Alex</p>
+            </Content>
+        </div>
     );
 }
 
